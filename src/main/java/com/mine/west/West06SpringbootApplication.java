da88e284e6658eb64be25c8a61e423b602912db2
Letoo <1171392006@qq.com>
2020-07-23 18:03:20 +0800

    2020-7-23

--- src/main/java/com/mine/west/West06SpringbootApplication.java
+++ src/main/java/com/mine/west/West06SpringbootApplication.java
@@ -3,10 +3,12 @@ package com.mine.west;
 import ch.qos.logback.core.spi.FilterReply;
 import com.mine.west.dao.RelayMapper;
 import com.mine.west.models.Relay;
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.SpringVersion;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,10 +21,14 @@ import java.util.List;
 //@RestController : = @Controller + @ResponseBody
 @MapperScan("com.mine.west.dao")
 @SpringBootApplication
+@Slf4j  //实现slf4j： log.info("233333");
 public class West06SpringbootApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(West06SpringbootApplication.class, args);
+        //System.out.println("!!!!"+SpringVersion.getVersion());
+        //log.info("233333");
     }
 
 }
--- src/test/java/com/mine/west/West06SpringbootApplicationTests.java
+++ src/test/java/com/mine/west/West06SpringbootApplicationTests.java
@@ -1,13 +1,27 @@
 package com.mine.west;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+
+
 
 @SpringBootTest
 class West06SpringbootApplicationTests {
+    //日志记录器：Logger
+    Logger logger = LoggerFactory.getLogger(getClass());
 
     @Test
     void contextLoads() {
+        //日志级别(可调整)：
+        //由低到高：trace < debug < info < warn < error
+        logger.trace("此为trace跟踪日志信息。。。");
+        logger.debug("此为debug调试日志信息。。。");
+        //springboot默认默认指定为info级别(root级别)，只输出>=info级别的日志
+        logger.info("此为info日志");
+        logger.warn("此为warn警告日志");
+        logger.error("此为error日志");
     }
 
 }
